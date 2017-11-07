663f6bee6606b12fdd9b8b90e31d6c28ae80d338
Iuri Mendes <fcoiuri@gmail.com>
2017-11-07 11:38:48 -0300

    Update Questao4.c

--- LISTA_01/Questao4.c
+++ LISTA_01/Questao4.c
@@ -7,10 +7,10 @@ int primo(int num){
     if(num==1){
       return 0;
     }
-    if(num==0){ 
+    if(num==0){
       return 0;
     }
-    
+
     int i;
     for(i=2; i<num; i++){//para numeros maiores que 2 verifica se é par e fim da funcao
         if(num%i==0){
@@ -29,36 +29,37 @@ int main(){
     if(!primo(num)){ // !operador lógico para negação
         printf("O número %d NÃO é primo\n", num);
     }
-        
-       
+
+
     if(primo(num)){
         printf("O número %d É primo\n", num);
         printf("Primos sucessores de %d: ", num);
-        for(i=num+1; total<2; i++){ 
+        for(i=num+1; total<2; i++){
             if(primo(i)){ //mostrando os dois proximos primos
-                total++; 
+                total++;
                 printf("%d, ", i);
             }
         }
 
         total=0;
-        printf("\nPrimos antecessores de %d: ", num); 
+        printf("\nPrimos antecessores de %d: ", num);
         for(i = num-1; i > 0; i--){
           if(primo(i)){ //mostrando os dois primos anteriores
-              printf("%d\n", i);
+              printf("%d, ", i);
               total++;
           }
-          if(total >= 2){ //só mostrar dois antecessores 
+          if(total >= 2){ //só mostrar dois antecessores
               break;
           }
-            
+
           if(num == 2){
             printf("Não possui antecessor\n");
           }
 
-  
+
         }
 
     };
+    printf("\n");
     return 0;
 }
